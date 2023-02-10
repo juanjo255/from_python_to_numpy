bc90ddeae73a29085278d695d7d3310bce38daab
Juan Picon <jjpiconc@eafit.edu.co>
2023-02-10 10:52:05 -0800

    breaking code down

--- boids.py
+++ boids.py
@@ -5,44 +5,45 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.collections import PathCollection
 
 
-class MarkerCollection:
-    """
-    Marker collection
-    """
+# class MarkerCollection:
+#     """
+#     Marker collection
+#     """
 
-    def __init__(self, n=100):
-        v = np.array([(-0.25, -0.25), (+0.0, +0.5), (+0.25, -0.25), (0, 0)])
-        c = np.array([Path.MOVETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY])
-        self._base_vertices = np.tile(v.reshape(-1), n).reshape(n, len(v), 2)
-        self._vertices = np.tile(v.reshape(-1), n).reshape(n, len(v), 2)
-        self._codes = np.tile(c.reshape(-1), n)
+#     def __init__(self, n=100):
+#         v = np.array([(-0.25, -0.25), (+0.0, +0.5), (+0.25, -0.25), (0, 0)])
+#         c = np.array([Path.MOVETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY])
+#         self._base_vertices = np.tile(v.reshape(-1), n).reshape(n, len(v), 2)
+#         self._vertices = np.tile(v.reshape(-1), n).reshape(n, len(v), 2)
+#         self._codes = np.tile(c.reshape(-1), n)
 
-        self._scale = np.ones(n)
-        self._translate = np.zeros((n, 2))
-        self._rotate = np.zeros(n)
+#         self._scale = np.ones(n)
+#         self._translate = np.zeros((n, 2))
+#         self._rotate = np.zeros(n)
 
-        self._path = Path(vertices=self._vertices.reshape(n*len(v), 2),
-                            codes=self._codes)
-        self._collection = PathCollection([self._path], linewidth=0.5,
-                                            facecolor="k", edgecolor="w")
+#         self._path = Path(vertices=self._vertices.reshape(n*len(v), 2),
+#                             codes=self._codes)
+#         self._collection = PathCollection([self._path], linewidth=0.5,
+#                                             facecolor="k", edgecolor="w")
 
-    def update(self):
-        n = len(self._base_vertices)
-        self._vertices[...] = self._base_vertices * self._scale
-        cos_rotate, sin_rotate = np.cos(self._rotate), np.sin(self._rotate)
-        R = np.empty((n, 2, 2))
-        R[:, 0, 0] = cos_rotate
-        R[:, 1, 0] = sin_rotate
-        R[:, 0, 1] = -sin_rotate
-        R[:, 1, 1] = cos_rotate
-        self._vertices[...] = np.einsum('ijk,ilk->ijl', self._vertices, R)
-        self._vertices += self._translate.reshape(n, 1, 2)
+#     def update(self):
+#         n = len(self._base_vertices)
+#         self._vertices[...] = self._base_vertices * self._scale
+#         cos_rotate, sin_rotate = np.cos(self._rotate), np.sin(self._rotate)
+#         R = np.empty((n, 2, 2))
+#         R[:, 0, 0] = cos_rotate
+#         R[:, 1, 0] = sin_rotate
+#         R[:, 0, 1] = -sin_rotate
+#         R[:, 1, 1] = cos_rotate
+#         self._vertices[...] = np.einsum('ijk,ilk->ijl', self._vertices, R)
+#         self._vertices += self._translate.reshape(n, 1, 2)
 
 
 class Flock:
     def __init__(self, count=500, width=640, height=360):
         self.width = width
         self.height = height
+        
         self.min_velocity = 0.5
         self.max_velocity = 2.0
         self.max_acceleration = 0.03
@@ -53,9 +54,14 @@ class Flock:
         self.velocity[:, 0] = np.cos(angle)
         self.velocity[:, 1] = np.sin(angle)
         angle = np.random.uniform(0, 2*np.pi, count)
-        radius = min(width, height)/2*np.random.uniform(0, 1, count)
+        radius = min(width, height)/2 * np.random.uniform(0, 1, count)
         self.position[:, 0] = width/2 + np.cos(angle)*radius
         self.position[:, 1] = height/2 + np.sin(angle)*radius
+        print(angle)
+        print()
+        print(self.position)
+        print ()
+        print(self.velocity)
 
     def run(self):
         position = self.position
@@ -151,7 +157,7 @@ class Flock:
 
 
 def update(*args):
-    global flock, collection, trace
+    global flock #, collection, trace
 
     # Flock updating
     flock.run()
@@ -162,37 +168,37 @@ def update(*args):
     collection.update()
 
     # Trace updating
-    if trace is not None:
-        P = flock.position.astype(int)
-        trace[height-1-P[:, 1], P[:, 0]] = .75
-        trace *= .99
-        im.set_array(trace)
+    # if trace is not None:
+    #     P = flock.position.astype(int)
+    #     trace[height-1-P[:, 1], P[:, 0]] = .75
+    #     trace *= .99
+    #     im.set_array(trace)
 
 
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    n = 500
-    width, height = 640, 360
+    n = 5
+    # width, height = 640, 360
     flock = Flock(n)
-    fig = plt.figure(figsize=(10, 10*height/width), facecolor="white")
-    ax = fig.add_axes([0.0, 0.0, 1.0, 1.0], aspect=1, frameon=False)
-    collection = MarkerCollection(n)
-    ax.add_collection(collection._collection)
-    ax.set_xlim(0, width)
-    ax.set_ylim(0, height)
-    ax.set_xticks([])
-    ax.set_yticks([])
+    # fig = plt.figure(figsize=(10, 10*height/width), facecolor="white")
+    # ax = fig.add_axes([0.0, 0.0, 1.0, 1.0], aspect=1, frameon=False)
+    # collection = MarkerCollection(n)
+    # ax.add_collection(collection._collection)
+    # ax.set_xlim(0, width)
+    # ax.set_ylim(0, height)
+    # ax.set_xticks([])
+    # ax.set_yticks([])
 
     # Trace
-    trace = None
-    if 0:
-        trace = np.zeros((height, width))
-        im = ax.imshow(trace, extent=[0, width, 0, height], vmin=0, vmax=1,
-                        interpolation="nearest", cmap=plt.cm.gray_r)
+    # trace = None
+    # if 0:
+    #     trace = np.zeros((height, width))
+    #     im = ax.imshow(trace, extent=[0, width, 0, height], vmin=0, vmax=1,
+    #                     interpolation="nearest", cmap=plt.cm.gray_r)
 
-    animation = FuncAnimation(fig, update, interval=10, frames=1000)
-    animation.save('boid.mp4', fps=40, dpi=80, bitrate=-1, codec="libx264",
-                    extra_args=['-pix_fmt', 'yuv420p'],
-                    metadata={'artist': 'Nicolas P. Rougier'})
-    plt.show()
+    # animation = FuncAnimation(fig, update, interval=10, frames=1000)
+    # animation.save('boid.mp4', fps=40, dpi=80, bitrate=-1, codec="libx264",
+    #                 extra_args=['-pix_fmt', 'yuv420p'],
+    #                 metadata={'artist': 'Nicolas P. Rougier'})
+    # plt.show()
